c1d3f9a4f9189a16443ca8b56b5052b76eedb17d
Philippe Santana Costa <philippescosta@gmail.com>
2017-09-19 23:12:22 -0300

    Add the options to invert transactions.

--- extrato-nubank.js
+++ extrato-nubank.js
@@ -1,6 +1,4 @@
-
 $(function() {
-
   function startOfx() {
     return `
 OFXHEADER:100
@@ -27,7 +25,6 @@ NEWFILEUID:NONE
 </STMTTRNRS>
 </BANKMSGSRSV1>
 </OFX>`;
-    
   }
 
   function bankStatement(date, amount, description) {
@@ -41,52 +38,67 @@ NEWFILEUID:NONE
   }
 
   function normalizeAmount(text) {
-    return text.replace('.', '').replace(',','.');
+    return text.replace(".", "").replace(",", ".");
   }
 
   function normalizeDay(date) {
-    return date.split(' ')[0]
+    return date.split(" ")[0];
   }
 
   function normalizeMonth(date) {
-    var month = date.split(' ')[1]
+    var month = date.split(" ")[1];
     var months = {
-      'Jan': '01',
-      'Fev': '02',
-      'Mar': '03',
-      'Abr': '04',
-      'Mai': '05',
-      'Jun': '06',
-      'Jul': '07',
-      'Ago': '08',
-      'Set': '09',
-      'Out': '10',
-      'Nov': '11',
-      'Dez': '12'
-    }
+      Jan: "01",
+      Fev: "02",
+      Mar: "03",
+      Abr: "04",
+      Mai: "05",
+      Jun: "06",
+      Jul: "07",
+      Ago: "08",
+      Set: "09",
+      Out: "10",
+      Nov: "11",
+      Dez: "12"
+    };
     return months[month];
   }
 
   function normalizeYear(date) {
-    var dateArray = date.split(' ');
+    var dateArray = date.split(" ");
     if (dateArray.length > 2) {
-      return '20'+dateArray[2];
-    } else { 
+      return "20" + dateArray[2];
+    } else {
       return new Date().getFullYear();
-    };
+    }
   }
 
   function normalizeDate(date) {
-    return normalizeYear(date)+normalizeMonth(date)+normalizeDay(date);
+    return normalizeYear(date) + normalizeMonth(date) + normalizeDay(date);
   }
 
-  function generateOfx() {
+  function generateOfx(opts) {
+    opts = opts || {};
     var ofx = startOfx();
 
-    $('.charge:visible').each(function(){
-      var date = normalizeDate($(this).find('.time').text());
-      var description = $(this).find('.description').text();
-      var amount = normalizeAmount($(this).find('.amount').text());
+    $(".charge:visible").each(function() {
+      var date = normalizeDate(
+        $(this)
+        .find(".time")
+        .text()
+      );
+      var description = $(this)
+        .find(".description")
+        .text();
+      var amount = normalizeAmount(
+        $(this)
+        .find(".amount")
+        .text()
+      );
+
+      if (opts.withInvertedAmount) {
+        amount = parseFloat(amount * -1).toFixed(2);
+      }
 
       ofx += bankStatement(date, amount, description);
     });
@@ -94,14 +106,27 @@ NEWFILEUID:NONE
     ofx += endOfx();
 
     link = document.createElement("a");
-    link.setAttribute("href", 'data:application/x-ofx,'+encodeURIComponent(ofx));
+    link.setAttribute(
+      "href",
+      "data:application/x-ofx," + encodeURIComponent(ofx)
+    );
     link.setAttribute("download", "fatura-nubank.ofx");
-    link.click();      
+    link.click();
   }
 
-  $(document).on('DOMNodeInserted', '.summary .nu-button:contains(Gerar boleto)', function () {
-    $('<button class="nu-button secondary" role="gen-ofx">Exportar OFX</button>')
-    .insertAfter('.summary .nu-button')
-    .click(generateOfx);
-  });
+  $(document).on(
+    "DOMNodeInserted",
+    ".summary .nu-button:contains(Gerar boleto)",
+    function() {
+      $(
+          '<button class="nu-button secondary" role="gen-ofx">Exportar OFX</button>'
+        )
+        .insertAfter(".summary .nu-button")
+        .click(function() {
+          generateOfx({
+            withInvertedAmount: confirm("Deseja que as transações sejam invertidas?")
+          })
+        });
+    }
+  );
 });
