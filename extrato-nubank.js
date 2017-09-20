fdaaf07020ba47989ddfec7af1261db180e22efa
Philippe Santana Costa <philippescosta@gmail.com>
2017-09-19 23:12:22 -0300

    Add the options to invert transactions.

--- extrato-nubank.js
+++ extrato-nubank.js
@@ -1,5 +1,5 @@
-$(function() {
-  function startOfx() {
+$(function () {
+  function startOfx () {
     return `
 OFXHEADER:100
 DATA:OFXSGML
@@ -15,118 +15,118 @@ NEWFILEUID:NONE
 <BANKMSGSRSV1>
 <STMTTRNRS>
 <STMTRS>
-<BANKTRANLIST>`;
+<BANKTRANLIST>`
   }
 
-  function endOfx() {
+  function endOfx () {
     return `
 </BANKTRANLIST>
 </STMTRS>
 </STMTTRNRS>
 </BANKMSGSRSV1>
-</OFX>`;
+</OFX>`
   }
 
-  function bankStatement(date, amount, description) {
+  function bankStatement (date, amount, description) {
     return `
 <STMTTRN>
 <TRNTYPE>OTHER</TRNTYPE>
 <DTPOSTED>${date}</DTPOSTED>
 <TRNAMT>${amount}</TRNAMT>
 <MEMO>${description}</MEMO>
-</STMTTRN>`;
+</STMTTRN>`
   }
 
-  function normalizeAmount(text) {
-    return text.replace(".", "").replace(",", ".");
+  function normalizeAmount (text) {
+    return text.replace('.', '').replace(',', '.')
   }
 
-  function normalizeDay(date) {
-    return date.split(" ")[0];
+  function normalizeDay (date) {
+    return date.split(' ')[0]
   }
 
-  function normalizeMonth(date) {
-    var month = date.split(" ")[1];
+  function normalizeMonth (date) {
+    var month = date.split(' ')[1]
     var months = {
-      Jan: "01",
-      Fev: "02",
-      Mar: "03",
-      Abr: "04",
-      Mai: "05",
-      Jun: "06",
-      Jul: "07",
-      Ago: "08",
-      Set: "09",
-      Out: "10",
-      Nov: "11",
-      Dez: "12"
-    };
-    return months[month];
+      Jan: '01',
+      Fev: '02',
+      Mar: '03',
+      Abr: '04',
+      Mai: '05',
+      Jun: '06',
+      Jul: '07',
+      Ago: '08',
+      Set: '09',
+      Out: '10',
+      Nov: '11',
+      Dez: '12'
+    }
+    return months[month]
   }
 
-  function normalizeYear(date) {
-    var dateArray = date.split(" ");
+  function normalizeYear (date) {
+    var dateArray = date.split(' ')
     if (dateArray.length > 2) {
-      return "20" + dateArray[2];
+      return '20' + dateArray[2]
     } else {
-      return new Date().getFullYear();
+      return new Date().getFullYear()
     }
   }
 
-  function normalizeDate(date) {
-    return normalizeYear(date) + normalizeMonth(date) + normalizeDay(date);
+  function normalizeDate (date) {
+    return normalizeYear(date) + normalizeMonth(date) + normalizeDay(date)
   }
 
-  function generateOfx(opts) {
-    opts = opts || {};
-    var ofx = startOfx();
+  function generateOfx (opts) {
+    opts = opts || {}
+    var ofx = startOfx()
 
-    $(".charge:visible").each(function() {
+    $('.charge:visible').each(function () {
       var date = normalizeDate(
         $(this)
-        .find(".time")
-        .text()
-      );
+          .find('.time')
+          .text()
+      )
       var description = $(this)
-        .find(".description")
-        .text();
+        .find('.description')
+        .text()
       var amount = normalizeAmount(
         $(this)
-        .find(".amount")
-        .text()
-      );
+          .find('.amount')
+          .text()
+      )
 
       if (opts.withInvertedAmount) {
-        amount = parseFloat(amount * -1).toFixed(2);
+        amount = parseFloat(amount * -1).toFixed(2)
       }
 
-      ofx += bankStatement(date, amount, description);
-    });
+      ofx += bankStatement(date, amount, description)
+    })
 
-    ofx += endOfx();
+    ofx += endOfx()
 
-    link = document.createElement("a");
+    var link = document.createElement('a')
     link.setAttribute(
-      "href",
-      "data:application/x-ofx," + encodeURIComponent(ofx)
-    );
-    link.setAttribute("download", "fatura-nubank.ofx");
-    link.click();
+      'href',
+      'data:application/x-ofx,' + encodeURIComponent(ofx)
+    )
+    link.setAttribute('download', 'fatura-nubank.ofx')
+    link.click()
   }
 
   $(document).on(
-    "DOMNodeInserted",
-    ".summary .nu-button:contains(Gerar boleto)",
-    function() {
+    'DOMNodeInserted',
+    '.summary .nu-button:contains(Gerar boleto)',
+    function () {
       $(
-          '<button class="nu-button secondary" role="gen-ofx">Exportar OFX</button>'
-        )
-        .insertAfter(".summary .nu-button")
-        .click(function() {
+        '<button class="nu-button secondary" role="gen-ofx">Exportar OFX</button>'
+      )
+        .insertAfter('.summary .nu-button')
+        .click(function () {
           generateOfx({
-            withInvertedAmount: confirm("Deseja que as transações sejam invertidas?")
+            withInvertedAmount: confirm('Deseja que as transações sejam invertidas?')
           })
-        });
+        })
     }
-  );
-});
+  )
+})
