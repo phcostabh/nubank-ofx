d22c8b84b35b9f707309480ee9cf97d92a72c965
Philippe Santana Costa <philippescosta@gmail.com>
2017-09-25 22:03:57 -0300

    Minor rafactorings, projetct package.json setup and build script.

--- extrato-nubank.js
+++ extrato-nubank.js
@@ -1,6 +1,6 @@
-$(function () {
-  function startOfx () {
-    return `
+const $ = require('jquery')
+
+const startOfx = `
 OFXHEADER:100
 DATA:OFXSGML
 VERSION:102
@@ -15,106 +15,106 @@ NEWFILEUID:NONE
 <BANKMSGSRSV1>
 <STMTTRNRS>
 <STMTRS>
-<BANKTRANLIST>`
-  }
+<BANKTRANLIST>
+`
 
-  function endOfx () {
-    return `
+const endOfx = `
 </BANKTRANLIST>
 </STMTRS>
 </STMTTRNRS>
 </BANKMSGSRSV1>
 </OFX>`
-  }
 
-  function bankStatement (date, amount, description) {
-    return `
+function bankStatement (date, amount, description) {
+  return `
 <STMTTRN>
 <TRNTYPE>OTHER</TRNTYPE>
 <DTPOSTED>${date}</DTPOSTED>
 <TRNAMT>${amount}</TRNAMT>
 <MEMO>${description}</MEMO>
-</STMTTRN>`
+</STMTTRN>
+`
+}
+
+function normalizeAmount (text = '') {
+  return text.replace('.', '').replace(',', '.')
+}
+
+function normalizeDay (date = '') {
+  return date.split(' ')[0]
+}
+
+function normalizeMonth (date = '') {
+  var month = date.split(' ')[1]
+  return {
+    Jan: '01',
+    Fev: '02',
+    Mar: '03',
+    Abr: '04',
+    Mai: '05',
+    Jun: '06',
+    Jul: '07',
+    Ago: '08',
+    Set: '09',
+    Out: '10',
+    Nov: '11',
+    Dez: '12'
+  }[month]
+}
+
+function normalizeYear (date = '') {
+  var dateArray = date.split(' ')
+  if (dateArray.length > 2) {
+    return '20' + dateArray[2]
   }
 
-  function normalizeAmount (text) {
-    return text.replace('.', '').replace(',', '.')
-  }
+  return new Date().getFullYear()
+}
 
-  function normalizeDay (date) {
-    return date.split(' ')[0]
-  }
+function normalizeDate (date) {
+  return normalizeYear(date) + normalizeMonth(date) + normalizeDay(date)
+}
 
-  function normalizeMonth (date) {
-    var month = date.split(' ')[1]
-    var months = {
-      Jan: '01',
-      Fev: '02',
-      Mar: '03',
-      Abr: '04',
-      Mai: '05',
-      Jun: '06',
-      Jul: '07',
-      Ago: '08',
-      Set: '09',
-      Out: '10',
-      Nov: '11',
-      Dez: '12'
-    }
-    return months[month]
-  }
-
-  function normalizeYear (date) {
-    var dateArray = date.split(' ')
-    if (dateArray.length > 2) {
-      return '20' + dateArray[2]
-    } else {
-      return new Date().getFullYear()
-    }
-  }
-
-  function normalizeDate (date) {
-    return normalizeYear(date) + normalizeMonth(date) + normalizeDay(date)
-  }
-
-  function generateOfx (opts) {
-    opts = opts || {}
-    var ofx = startOfx()
-
-    $('.charge:visible').each(function () {
-      var date = normalizeDate(
-        $(this)
-          .find('.time')
-          .text()
-      )
-      var description = $(this)
-        .find('.description')
+function generateOfx (opts) {
+  opts = opts || {}
+  var ofx = ''
+  $('.charge:visible').each(function () {
+    var date = normalizeDate(
+      $(this)
+        .find('.time')
         .text()
-      var amount = normalizeAmount(
-        $(this)
-          .find('.amount')
-          .text()
-      )
-
-      if (opts.withInvertedAmount) {
-        amount = parseFloat(amount * -1).toFixed(2)
-      }
-
-      ofx += bankStatement(date, amount, description)
-    })
-
-    ofx += endOfx()
-
-    var link = document.createElement('a')
-    link.setAttribute(
-      'href',
-      'data:application/x-ofx,' + encodeURIComponent(ofx)
     )
-    link.setAttribute('download', 'fatura-nubank.ofx')
-    link.click()
-  }
+    var description = $(this)
+      .find('.description')
+      .text()
+    var amount = normalizeAmount(
+      $(this)
+        .find('.amount')
+        .text()
+    )
 
-  $(document).on(
+    if (opts.withInvertedAmount) {
+      amount = parseFloat(amount * -1).toFixed(2)
+    }
+
+    ofx += bankStatement(date, amount, description)
+  })
+
+  ofx += endOfx()
+
+  var link = document.createElement('a')
+  link.setAttribute(
+    'href',
+    'data:application/x-ofx,' + encodeURIComponent(startOfx + ofx + endOfx)
+  )
+  link.setAttribute('download', 'fatura-nubank.ofx')
+  link.click()
+}
+
+$(function () {
+  $(
+    document
+  ).on(
     'DOMNodeInserted',
     '.summary .nu-button:contains(Gerar boleto)',
     function () {
@@ -123,9 +123,12 @@ NEWFILEUID:NONE
       )
         .insertAfter('.summary .nu-button')
         .click(function () {
-          generateOfx({
-            withInvertedAmount: confirm('Deseja que as transações sejam invertidas?')
-          })
+          var opts = {
+            withInvertedStatements: window.confirm(
+              'Deseja que as transações sejam invertidas?'
+            )
+          }
+          generateOfx(opts)
         })
     }
   )
